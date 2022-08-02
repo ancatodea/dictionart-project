59444592a76541aecc18fed19ffd33b63c82f11b
ancatodea <103606786+ancatodea@users.noreply.github.com>
2022-08-02 14:15:37 +0200

    Created the search engine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         hello
       </header>
+      <Dictionary />
+      </div>
     </div>
   );
 }
