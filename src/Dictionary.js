594acf5c5d618a6db5c65a8eac5e7ac6e54a2455
ancatodea <103606786+ancatodea@users.noreply.github.com>
2022-08-03 14:05:56 +0200

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,9 +2,10 @@ import React, {useState} from "react";
 
 export default function Dictionary () {
   let [keyword, setKeyword] = useState("");
-  
+
   function search(event) {
     event.preventDefault();
+    alert (`Searching for ${keyword}`);
   }
 
   function handleKeywordChange(event) {
