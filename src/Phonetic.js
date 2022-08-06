3a8805f8142640173320d792168a79b87ee6fe69
ancatodea <103606786+ancatodea@users.noreply.github.com>
2022-08-06 20:34:44 +0200

    Updated phonetic component

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -2,12 +2,13 @@ import React from "react";
 import "./Phonetic.css"
 
 export default function Phonetic(props) {
+  
   return (
     <div className="Phonetic">
+      <a href={[props.phonetic.audio]} target="_blank" rel="noopener noreferrer">Listen</a>
       <span className="text">
        {props.phonetic.text}
       </span>
-      <a href={[props.phonetic.audio]} target="_blank" rel="noopener noreferrer">Listen</a>
     </div>
   );
 }
